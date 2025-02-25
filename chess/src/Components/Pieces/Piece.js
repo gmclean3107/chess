5296d0fb6ee052907ff96404851e4acb03035433
Gerard McLean <gmclean09@qub.ac.uk>
2025-02-25 23:03:01 +0000

    Drag and Drop functionality implemented

--- chess/src/Components/Pieces/Piece.js
+++ chess/src/Components/Pieces/Piece.js
@@ -1,8 +1,21 @@
 import "./Pieces.css";
 
 const Piece = ({rank, file, piece}) => {
+
+    const onDragStart = (e) => {
+        e.dataTransfer.effectAllowed = 'move';
+        e.dataTransfer.setData('text/plain', `${piece},${rank},${file}`);
+        setTimeout(() => {
+            e.target.style.display = 'none';
+        }, 0);
+    }
+
     return(
-        <div className={`piece ${piece} p-${file}${rank}`}/>
+        <div 
+            className={`piece ${piece} p-${file}${rank}`}
+            draggable={true}
+            onDragStart={onDragStart}
+        />
     )
 }
 
--- chess/src/Components/Pieces/Pieces.js
+++ chess/src/Components/Pieces/Pieces.js
@@ -1,42 +1,52 @@
 import "./Pieces.css";
 import Piece from "./Piece";
+import { useState, useRef } from "react";
+import { createPosition, copyPos } from "../../helper";
 
 const Pieces = () => {
-    const position = new Array(8).fill('').map(x => new Array(8).fill(''));
 
-    for(let i=0; i<8; i++) {
-        position[1][i] = 'wp';
-        position[6][i] = 'bp';
+    const ref = useRef();
+
+    const [state, setState] = useState(createPosition());
+
+    const calculateCoords = (e) => {
+        const {top,left,width} = ref.current.getBoundingClientRect();
+        const size = width/8;
+        const y = Math.floor((e.clientX-left)/size) ;
+        const x = 7-Math.floor((e.clientY-top)/size);
+
+        return {x,y};
     }
 
-    position[0][0] = 'wr';
-    position[0][1] = 'wn';
-    position[0][2] = 'wb';
-    position[0][3] = 'wq';
-    position[0][4] = 'wk';
-    position[0][5] = 'wb';
-    position[0][6] = 'wn';
-    position[0][7] = 'wr';
+    const onDrop = (e) => {
+        const newPos = copyPos(state);
+        const {x, y} = calculateCoords(e);
+        const [piece, rank, file] = e.dataTransfer.getData('text').split(',');
 
-    position[7][0] = 'br';
-    position[7][1] = 'bn';
-    position[7][2] = 'bb';
-    position[7][3] = 'bq';
-    position[7][4] = 'bk';
-    position[7][5] = 'bb';
-    position[7][6] = 'bn';
-    position[7][7] = 'br';
+        newPos[rank][file] = '';
+        newPos[x][y] = piece;
+
+        setState(newPos);
+    }
+
+    const onDragOver = (e) => {
+        e.preventDefault();
+    }
 
     return(
-        <div className='pieces'>
-            {position.map((r, rank) => 
+        <div 
+            ref={ref}
+            className='pieces'
+            onDrop={onDrop}
+            onDragOver={onDragOver}>
+            {state.map((r, rank) => 
                 r.map((f, file) => 
-                    position[rank][file]
+                    state[rank][file]
                     ? <Piece
                         key = {rank+'-'+file}
                         rank = {rank}
                         file = {file}
-                        piece = {position[rank][file]}
+                        piece = {state[rank][file]}
                         /> 
                         : null
             ))}
